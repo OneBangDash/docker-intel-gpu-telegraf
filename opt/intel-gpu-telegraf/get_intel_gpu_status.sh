031b8c4f4a54aa095739693d707611ae8bb2f93e
Scott Peters <OneBangDash@gmail.com>
2024-01-03 10:10:52 -0500

    add blender, blitter, video enhance metrics

--- opt/intel-gpu-telegraf/get_intel_gpu_status.sh
+++ opt/intel-gpu-telegraf/get_intel_gpu_status.sh
@@ -4,10 +4,13 @@
 
 #Beat intel_gpu_top into submission
 JSON=$(/usr/bin/timeout -k 3 3 /usr/bin/intel_gpu_top -J)
-VIDEO_UTIL=$(echo "$JSON"|grep Video/0 -A 1|tail -1|grep busy|cut -d ":" -f2|cut -d "," -f1|cut -d " " -f2)
+VIDEO_UTIL=$(echo "$JSON"|grep '"Video":' -A 1 | tail -1 | grep busy | cut -d ":" -f2 | cut -d "," -f1 | cut -d " " -f2)
+RENDER_UTIL=$(echo "$JSON"|grep '"Render/3D":' -A 1 | tail -1 | grep busy | cut -d ":" -f2 | cut -d "," -f1 | cut -d " " -f2)
+BLITTER_UTIL=$(echo "$JSON"|grep '"Blitter":' -A 1 | tail -1 | grep busy | cut -d ":" -f2 | cut -d "," -f1 | cut -d " " -f2)
+VIDENHANCE_UTIL=$(echo "$JSON"|grep '"VideoEnhance":' -A 1 | tail -1 | grep busy | cut -d ":" -f2 | cut -d "," -f1 | cut -d " " -f2)
 
 #Spit out something telegraf can work with
-echo "[{\"time\": $(date +%s), \"intel_gpu_util\": "$VIDEO_UTIL"}]"
+echo "[{\"time\": $(date +%s), \"intel_gpu_video\": "$VIDEO_UTIL", \"intel_gpu_render\": "$RENDER_UTIL", \"intel_gpu_blitter\": "$BLITTER_UTIL", \"intel_gpu_video_enhance\": "$VIDENHANCE_UTIL"}]"
 
 #Exit cleanly
 exit 0
